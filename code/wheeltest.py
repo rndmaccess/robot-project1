76764e7a08f17d1f118c9cf0415e96c1a6f9bf83
John <johnjamisonfoth@gmail.com>
2026-02-11 12:22:43 -0700

    push for testing

--- code/wheeltest.py
+++ code/wheeltest.py
@@ -1,9 +1,19 @@
 # motor channels = 14 & 15
 
 from robot import Robot
-
+import time
 
 r = Robot()
 
 
-r.
+r.drive_wheels(1200)
+
+time.sleep(1.0)
+
+r.drive_wheels(1800)
+
+time.sleep(1.0)
+
+r.drive_wheels(1200)
+
+
