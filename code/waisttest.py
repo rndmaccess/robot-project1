9e89f86007b3de8f6cb9af547a9c7a19c15f2e1b
robot17@rasberrypy.com <robot17@rasberrypy.com>
2026-02-12 13:52:25 -0700

    got the waist to work

--- code/waisttest.py
+++ code/waisttest.py
@@ -4,16 +4,20 @@ from robot import Robot
 import time
 
 r = Robot()
-speed = 8000
-
-r.rotate_waist(6000)
-i = input()
+speed = 0
 
 
-r.rotate_waist(speed)
-print("Speed = " + str(i))
 
-i = input()
+i = "y"
+
+while((i == "n") == False):
+
+    print("please enter a speed (n to quit):")
+    i = input()
+    speed = int(i)
+
+    r.rotate_waist(speed)
+
 
 r.rotate_waist(6000)
 r.close()
